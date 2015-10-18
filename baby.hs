bb87738b37887289a4a79c9edf77af258cdacd8c
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-10-18 16:34:12 -0700

    list comprehensions in haskell

--- baby.hs
+++ baby.hs
@@ -1,3 +1,6 @@
 doubleMe x = x + x
 doubleUs x y = x*2 + y*2
 doubleSmallNumber x = if x > 100 then x else x*2
+boomBangs xs = [if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
+length' xs = sum [1 | _ <- xs]
+removeNonUppercase st = [ c | c <- st, c 'elem' ['A'..'Z']]
